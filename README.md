b8cd2d89817c65d8fd55ad8f8bf4194d8b676ca3
Chrisi Hendricks <hendricks.christina@gmail.com>
2016-06-08 21:25:54 -0400

    Add 5 presidents objects to fill the pres array

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 
+# Links
+
+* [Wikipedia.com] (https://en.wikipedia.org/wiki/List_of_Presidents_of_the_United_States)
+I used this site to find the information about the presidents.
--- js/main.js
+++ js/main.js
@@ -1,2 +1,46 @@
 var iceCream = ['Strawberry', 'Cookies and Cream', 'Vanilla',
     'Mint Chip'];
+
+var president44 = {
+    firstName: 'Barack',
+    lastName: 'Obama',
+    termLength: '8 years',
+    party: 'Democrat',
+    yearsOfPresidency: '2009 - 2016'
+};
+
+var president43 = {
+    firstName: 'George W.',
+    lastName: 'Bush',
+    termLength: '8 years',
+    party: 'Republican',
+    yearsOfPresidency: '2001 - 2009'
+};
+
+var president42 = {
+    firstName: 'Bill',
+    lastName: 'Clinton',
+    termLength: '8 years',
+    party: 'Democrat',
+    yearsOfPresidency: '1993 - 2001'
+};
+
+var president41 = {
+    firstName: 'George H. W.',
+    lastName: 'Bush',
+    termLength: '4 years',
+    party: 'Republican',
+    yearsOfPresidency: '1989 - 1993'
+};
+
+var president40 = {
+    firstName: 'Ronald',
+    lastName: 'Reagan',
+    termLength: '8 years',
+    party: 'Republican',
+    yearsOfPresidency: '1981 - 1989'
+};
+
+var presidents = [president44, president43, president42, president41, president40];
+
+console.log(presidents[2].firstName, presidents[2].lastName);
